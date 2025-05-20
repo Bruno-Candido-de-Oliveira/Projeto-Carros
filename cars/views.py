f1422bf63f1cc234ca4141d354e1070fa5f43191
Bruno-Candido-de-Oliveira <bruno_candido18@yahoo.com>
2025-05-19 23:47:23 -0300

    Adicionado filtros de busca e interface aprimorada

--- cars/views.py
+++ cars/views.py
@@ -3,11 +3,15 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model') #Define a variável cars como o retorno de todos os objetos da classe cars por meio da função 'all', ou seja, será atribuiído a ela todos os carros criados
+    search = request.GET.get('search') #recebe a busca do usuário
+
+    if search: #se a busca do usuário for verdadeira, exibirá o conteúdo conforme a solicitação
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
 
 
-    return render(
-        request, 'cars.html',
+    return render( #no retorno da função, será exibido na tela do usuário o contúdo da variável car
+        request, 'cars.html', 
         {'cars': cars}
     )
 
